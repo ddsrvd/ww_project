141143af514cfe51b4c8e4e621dea03dc4c5c918
kirillrocket <kirill27032943@gmail.com>
2025-12-11 16:13:13 +0300

    Update States.py

--- bot/app/States.py
+++ bot/app/States.py
@@ -2,3 +2,7 @@ from aiogram.fsm.state import StatesGroup, State
 class Create(StatesGroup):
     get_name = State()
     get_comment = State()
+
+class Search(StatesGroup):
+    s_name = State()
+    s_author = State()
