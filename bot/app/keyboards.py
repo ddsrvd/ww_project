38fec51037a32c884aae8695a4116302f497ee30
kirillrocket <kirill27032943@gmail.com>
2025-12-13 14:13:23 +0300

    Update keyboards.py

--- bot/app/keyboards.py
+++ bot/app/keyboards.py
@@ -1,13 +1,25 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
-#Reply клавиатура
+
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Найти трек")],
     [KeyboardButton(text="Добавить трек")],
-],resize_keyboard = True)
+    [KeyboardButton(text="Написать/Посмотреть комментарий")],
+], resize_keyboard=True)
 
 find = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Поиск по автору")],
     [KeyboardButton(text="Поиск по треку")],
-],resize_keyboard = True)
+    [KeyboardButton(text="назад")],
+], resize_keyboard=True)
+
+none_author = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="нет автора")],
+], resize_keyboard=True)
+
+answer = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Написать комментарий")],
+    [KeyboardButton(text="Посмотреть комментарии")],
+    [KeyboardButton(text="Другой трек")],
+], resize_keyboard=True)
 
